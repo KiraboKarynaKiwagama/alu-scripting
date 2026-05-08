f187a66646e35fe85d96f529d95440ddf4a35009
KiraboKarynaKiwagama <k.kiwagama@alustudent.com>
2026-05-08 23:55:52 +0200

    yoh

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -1,2 +1,3 @@
+#!/usr/bin/env ruby
 # Scans the log for [from:], [to:], and [flags:] patterns, extracts the sender, receiver, and flags, and prints them in the format SENDER,RECEIVER,FLAGS
 puts ARGV[0].scan(/\[from:([^\]]+)\] \[to:([^\]]+)\] \[flags:([^\]]+)\]/).map { |match| match.join(',') }.join("\n")
